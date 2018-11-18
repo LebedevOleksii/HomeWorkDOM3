37bfc72dcc281d200870a0c459a91c6685592b23
Oleksii Lebedev <lebediew.oleksii@gmail.com>
2018-11-18 21:37:58 +0200

    new fixe

--- hw15.js
+++ hw15.js
@@ -5,16 +5,16 @@ class Button {
             width = 30,
             height = 50,
             parentElement = document.body,
-            innerHTML = 'button',
+            innerText = 'button',
             opacity =  0.5,
             onClick = ()=>{}
         } = options;
         this.button = document.createElement('button');
         this.button.style.width = `${width}px`;
         this.button.style.height = `${height}px`;
-        this.button.innerHTML = innerHTML;
+        this.button.innerText = innerText;
         this.button.style.opacity = opacity;
-        this.button.style.border = '1px solid #333';
+        this.button.style.border = '1px solid #D3D3D3';
         this.button.style.padding = 0;
         this.button.style.font = 'inherit';
         this.button.style.color = 'inherit';
@@ -22,29 +22,26 @@ class Button {
         this.button.classList.add('myButton');
         
         this.button.addEventListener('mouseup', (event)=>{
-            if( event.clientX > 608 && event.clientX < 717 && event.clientY > 120 && event.clientY < 139 ) {
-                this.transitionButton('all 2s ease-in-out .5s');
-                this.transformButton('rotateX(0deg)');
-                this.transformButton('rotateX(-45deg)');
-                this.transformButton('rotateX(-180deg)');
+            if( 608 < event.clientX  < 717 && 120 < event.clientY < 139 ) {
+                this.button.style.background = '#6495ED';
+                this.transitionButton('1.5s ease-in-out ');
+                this.transformButton('rotateX(-10deg)');
                 this.transformButton('rotateX(-360deg)');
+                this.button.style.font = '1.3em Ariel';
+                this.button.style.boxShadow = '4px 4px 12px -5px #000000, 4px 4px 14px -3px #ffffff inset';
+                setTimeout(()=>{
+                    this.opacityButton(1);
+                    this.button.innerText = 'натиснуто';
+                }, 500);
             }
         });
-
-        
         this.appenToElement(parentElement);
     }
     appenToElement(element){
-        if(!(element instanceof HTMLElement)){ 
-            throw new ERROR ("Перший елемент. повинен бути HTML елементом");
-        }
         element.append(this.button);
     }
     changeInnerHTML(innerHtml){
         this.button.innerHTML(innerHtml);
-    }
-    animateButton(scale){
-        this.button.style.transform = `scale(${scale})`;
     }
     transitionButton(transition){
         this.button.style.transition = transition;
@@ -55,12 +52,11 @@ class Button {
     transformButton(value){
         this.button.style.transform = value;
     }
-    
 }
 
 const testButton = new Button ({
     width: 110,
     height: 40,
-    innerHTML: "кнопка",
+    innerText: "натиснути",
     parentElement: document.querySelector('.wrapper')
 });
